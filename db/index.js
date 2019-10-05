29794b02d7882a910e857aa8fb3bb51673264cf6
Samuel Adewole <samueladewole15@gmail.com>
2019-10-05 23:53:35 +0100

    version --check

--- db/index.js
+++ db/index.js
@@ -9,7 +9,9 @@ const connectDB = async () => {
     try {
         // connect to mongodb
         await mongoose.connect(mongoURI, {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useCreateIndex: true
         });
 
         console.log('Connected to DB')
