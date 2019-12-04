a47e01bccfc2f30cc6986c2213576124dd51e2b3
Samuel Adewole <samueladewole15@gmail.com>
2019-12-04 22:42:57 +0100

    checked version

--- db/index.js
+++ db/index.js
@@ -8,7 +8,7 @@ const mongoURI = process.env.mongoURI
 const connectDB = async () => {
     try {
         // connect to mongodb
-        await mongoose.connect('mongodb+srv://devConnect:devcon12345@cluster0-seeyl.mongodb.net/test?retryWrites=true&w=majority', {
+        await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useFindAndModify: false,
             useCreateIndex: true,
